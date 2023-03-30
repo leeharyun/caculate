2acb3016b932b6108fd351316bd2b1dce9d5d99c
leeharyun <gkfbs3399@naver.com>
2023-03-31 03:51:36 +0900

    html 및 계산 로직 생성

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-export {};
+import * as calculate from './js/calculate';
+
+export { calculate };
---
+++ src/js/calculate.ts
@@ -0,0 +1,139 @@
+const calculateState = () => {
+  let result: number = 0;
+  let operator: string = '';
+
+  let prevCount = 0;
+  let computedCount = 0;
+
+  const changeResult = () => {
+    const reslutSection = document.querySelector('.result-section') as HTMLElement;
+
+    if (reslutSection) {
+      reslutSection.textContent = result.toString();
+    }
+  };
+
+  const initResult = () => {
+    result = 0;
+    operator = '';
+    changeResult();
+  };
+
+  const onClickNumberButtons = (e: Event) => {
+    e.preventDefault();
+
+    const target = e.target as HTMLElement;
+    const targetValue = target.dataset.value;
+
+    if (!targetValue) {
+      return;
+    }
+
+    if (result > 0 && !operator) {
+      result = Number(result.toString() + targetValue);
+    } else {
+      result = Number(targetValue);
+    }
+
+    changeResult();
+  };
+
+  const convertResultNumberType = () => {
+    if (result > 0) {
+      result = result * -1;
+    } else {
+      result = Math.abs(result);
+    }
+
+    changeResult();
+  };
+
+  const convertResultPercent = () => {
+    result = result / 100;
+    changeResult();
+  };
+
+  const onClickOperatorButton = (e: Event) => {
+    const target = e.target as HTMLElement;
+    const targetValue = target.dataset.value;
+
+    if (!targetValue || !result) {
+      return;
+    }
+
+    const isInit = targetValue === 'C';
+    const convertNumberType = targetValue === '+/-';
+    const isConvertPercent = targetValue === '%';
+
+    const isAddition = targetValue === '+';
+    const isSubtraction = targetValue === '-';
+    const isMultiplication = targetValue === 'X';
+    const isDivision = targetValue === '/';
+    const isComputedResult = targetValue === '=';
+
+    if (isInit) {
+      initResult();
+      return;
+    }
+
+    if (convertNumberType) {
+      convertResultNumberType();
+      return;
+    }
+
+    if (isConvertPercent) {
+      convertResultPercent();
+      return;
+    }
+
+    if (isAddition || isSubtraction || isMultiplication || isDivision) {
+      operator = targetValue;
+      prevCount = result;
+    }
+
+    if (isComputedResult) {
+      const operatorIsAdd = operator === '+';
+      const operatorIsSub = operator === '-';
+      const operatorIsMult = operator === 'X';
+      const operatorIsDiv = operator === '/';
+
+      if (operatorIsAdd) {
+        computedCount = prevCount + result;
+      }
+      if (operatorIsSub) {
+        computedCount = prevCount - result;
+      }
+      if (operatorIsMult) {
+        computedCount = prevCount * result;
+      }
+      if (operatorIsDiv) {
+        computedCount = prevCount / result;
+      }
+
+      result = computedCount;
+      changeResult();
+      return;
+    }
+  };
+
+  return {
+    onClickNumberButtons,
+    onClickOperatorButton,
+  };
+};
+
+const buttonsAddClickEvent = (selector: string, event: (e: Event) => void) => {
+  const buttons = document.querySelectorAll(selector);
+  buttons.forEach((button) => button.addEventListener('click', event));
+};
+
+const DEFAULT_BUTTONS_SELECTOR = '.calculator-container .buttons-section .button-table tr td';
+
+window.onload = () => {
+  const calculate = calculateState();
+
+  buttonsAddClickEvent(`${DEFAULT_BUTTONS_SELECTOR}.number`, calculate.onClickNumberButtons);
+  buttonsAddClickEvent(`${DEFAULT_BUTTONS_SELECTOR}.operator`, calculate.onClickOperatorButton);
+};
+
+export {};
